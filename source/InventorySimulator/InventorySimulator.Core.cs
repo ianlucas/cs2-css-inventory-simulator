4d68b633686cd30ef1b92c6648e7ce73ae6f7f2c
Ian Lucas <ianlucas_ferreira@hotmail.com>
2026-03-10 18:40:02 -0300

    Fix FormatException when weaponItemId is empty

--- source/InventorySimulator/InventorySimulator.Core.cs
+++ source/InventorySimulator/InventorySimulator.Core.cs
@@ -110,9 +110,10 @@ public partial class InventorySimulator
         if (
             weapon == null
             || !weapon.HasCustomItemID()
+            || !ulong.TryParse(weaponItemId, out var parsedItemId)
             || weapon.AttributeManager.Item.AccountID
                 != new CSteamID(player.SteamID).GetAccountID().m_AccountID
-            || weapon.AttributeManager.Item.ItemID != ulong.Parse(weaponItemId)
+            || weapon.AttributeManager.Item.ItemID != parsedItemId
         )
             return;
         var inventory = player.GetState().Inventory;
